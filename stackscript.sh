e052634842fb61acfc9b37ac07a3a07682caf93f
yixinglu <yxlyee@gmail.com>
2017-10-11 13:53:50 +0800

    fix error

--- stackscript.sh
+++ stackscript.sh
@@ -28,7 +28,7 @@ service supervisor start
 
 supervisorctl reload
 
-sysctl net.ipv4.tcp_available_congestion_control
+# sysctl net.ipv4.tcp_available_congestion_control
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
